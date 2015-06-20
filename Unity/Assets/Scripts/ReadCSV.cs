66b44a05566bea65df63115c68bc7f2d60b51ac4
amin_manna <amin10@meet.mit.edu>
2015-06-20 17:28:59 -0400

    it is a beauty

--- Unity/Assets/Scripts/ReadCSV.cs
+++ Unity/Assets/Scripts/ReadCSV.cs
@@ -6,74 +6,57 @@ using System.Collections.Generic;
 
 public class ReadCSV : MonoBehaviour {
 	
-	public Dictionary<string, Color> instruments = new Dictionary<string, Color>
-	{
-		{"acoustic guitar", Color.red},
-		{"female singer", Color.blue},
-		{"clean electric guitar", Color.green}
+	public Dictionary<string, Color> instruments = new Dictionary<string, Color>();
+	public string[] csvfiles = {
+		"rainfall.csv", "phoenix.csv"
 	};
-	
+	public Color[] colors = {
+		Color.red, Color.green, Color.blue,  Color.yellow, Color.cyan, Color.black, Color.magenta
+	};
+	public int currentColor= 0;
 	
 	void Start ()
 	{
-		string fileData = System.IO.File.ReadAllText(Application.dataPath+"/CSV/rainfall.csv");
-		string[] lines = fileData.Split("\n"[0]);
-	
-		//Regex startRegex = new Regex( @"^\d+\.\d+");
-		//Regex endRegex = new Regex( @"(?<=^\d+\.\d+,{1})\d+\.\d+");
-		//Regex instrumentRegex = new Regex (@"\w+\s+\w+(s\+)");
+		for (int j = 0; j < csvfiles.Length; j++) {
+			print(j);
 		
+			string fileData = System.IO.File.ReadAllText (Application.dataPath + "/CSV/"+csvfiles[j]);
+			string[] lines = fileData.Split ("\n" [0]);
 		
-		for (int i = 1; i < lines.Length - 1; i++)
-		{
-			string[] items = lines[i].Split (","[0]);
+			//Regex startRegex = new Regex( @"^\d+\.\d+");
+			//Regex endRegex = new Regex( @"(?<=^\d+\.\d+,{1})\d+\.\d+");
+			//Regex instrumentRegex = new Regex (@"\w+\s+\w+(s\+)");
 			
-			//print ("start: " + items[0] + " end: " + items[1]);
-			GameObject cube = Instantiate(Resources.Load ("Prefabs/Cube")) as GameObject;
-			cube.GetComponent<MeshRenderer>().material.color = instruments[items[2]];
-			cube.transform.localScale = new Vector3 (1,1, float.Parse (items[1]) - float.Parse (items[0]));
 			
-			switch (items[2])
-			{
+			for (int i = 1; i < lines.Length-1; i++) {
+				string[] items = lines [i].Split ("," [0]);
+
+				//print ("start: " + items[0] + " end: " + items[1]);
+				GameObject cube = Instantiate (Resources.Load ("Prefabs/Cube")) as GameObject;
+				if( !instruments.ContainsKey(items[2])){
+					instruments.Add(items[2], colors[currentColor]);
+					currentColor = (currentColor + 1)% colors.Length;
+				}
+				cube.GetComponent<MeshRenderer> ().material.color = instruments [items [2]];
+				cube.transform.localScale = new Vector3 (1, 1, float.Parse (items [1]) - float.Parse (items [0]));
+				
+				switch (items [2]) {
 				case "acoustic guitar":
-					cube.transform.position = new Vector3(0,1, float.Parse(items[0]));
+					cube.transform.position = new Vector3 (j, 1, float.Parse (items [0]));
 					break;
-					
+						
 				case "female singer":
-					cube.transform.position = new Vector3(0,2, float.Parse(items[0]));
+					cube.transform.position = new Vector3 (j, 2, float.Parse (items [0]));
 					break;
-					
+						
 				case "clean electric guitar":
-              		cube.transform.position = new Vector3(0,3, float.Parse(items[0]));
-             	 	break;
+					cube.transform.position = new Vector3 (j, 3, float.Parse (items [0]));
+					break;
+				}
+
+				
+
 			}
-			
-			//print (items[2]);
-			
-//			Match startMatch = startRegex.Match (lines[i]);
-//			Match endMatch = endRegex.Match (lines[i]);
-//			Match instrumentMatch = instrumentRegex.Match (lines[i]);
-//			
-//			if (startMatch.Success)
-//			{
-//				GameObject cube = Instantiate(Resources.Load ("Prefabs/Cube"), new Vector3(0,0, float.Parse(startMatch.Value)), Quaternion.identity) as GameObject;
-//				cube.transform.localScale = new Vector3 (1,1, float.Parse (endMatch.Value) - float.Parse (startMatch.Value));
-//				
-//				print (instrumentMatch.Value);
-//				//print(instruments[instrumentMatch.Value]);
-//				
-////				switch (instrumentMatch.Value)
-////				{
-////					case 
-////				}
-//				
-//				//print (instrumentMatch.Value);
-//			}
 		}
-		
-		
-		//string[] lineData  = (lines[0].Trim()).Split(","[0]);
-		//float x;
-		//float.TryParse(lineData[0], out x);
 	}
 }
